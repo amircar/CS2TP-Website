6917c4f32c4d2fb6b72e3a837133aac283d09915
Hanin Nabeel <230087860@aston.ac.uk>
2024-11-22 11:54:34 +0000

    Homepage Styling
    Started adding more styles to the homepage, making it look like the mock up website we created

--- homepage.php
+++ homepage.php
@@ -3,9 +3,82 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel ="stylesheet" type="text/css" href="css/styles.css" />
+    <title>Homepage</title>
+    <style type="text/css">
+
+    </style>
 </head>
 <body>
+    <!-- This is the code for the top nav with logo, search, sign in, wishlist and basket -->
+    <div id="top-nav">
+    <div class="logo">
+            <img src="logo.png" alt="Team 47 Logo">
+        </div>
+        <div id="search-container"></div>
+        <button class="signin" style="text-align: center;">Sign In</button>
+        <div id="heart">
+            <i class="fas fa-heart"></i>
+        </div>
+        <div id="cart">
+            <i class="fas fa-shopping-cart"></i>
+        </div>
+    </div>
+    <nav>
+    <!-- The nav bar -->
+        <ul> 
+            <header class="navigation">
+        <div class="nav">
+          <li><a href="">Men</a></li>
+          <li><a href="">Women</a></li>
+          <li><a href="">Kids</a></li>
+          <li><a href="">Accessories</a></li>
+          <li><a href="">About Us</a></div></li>
+        </header>
+        </ul> 
+    </nav>
+
+
+    <div id="container">
+        <div id="section"></div>
+
+        <div id="section" style="background-color: rgb(238, 238, 238); height: 800px;">
+            <div id="subheadingbox">
+                <div class="triangle"></div>
+                <h3>Trending</h3>
+        </div>
+        </div>
+
+
+
+
+        <div id="section">
+            <div class="trapezoid1"></div>
+            <div class="trapezoid1-rectangle">
+                <button class="trapezoid-button" style="text-align: center; clip-path: polygon(0% 0%, 100% 0%, 95% 100%, 0% 100%);">MEN</button>
+            </div>
+            
+            <div class="trapezoid2"></div>
+            <div class="trapezoid2-rectangle">
+                <button class="trapezoid-button" style="text-align: center; width: 315px;">WOMEN</button>
+            </div>
+
+            <div class="trapezoid3"></div>
+            <div class="trapezoid3-rectangle">
+            <button class="trapezoid-button" style="text-align: center; clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 5% 100%)">KIDS</button>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+        
     
 </body>
 </html>

992448a98801728b0f42fd40a14e1011271350ce
Hanin Nabeel <230087860@aston.ac.uk>
2024-11-26 23:05:48 +0000

    new navbar page

--- homepage.php
+++ homepage.php
@@ -11,34 +11,7 @@
     </style>
 </head>
 <body>
-    <!-- This is the code for the top nav with logo, search, sign in, wishlist and basket -->
-    <div id="top-nav">
-    <div class="logo">
-            <img src="logo.png" alt="Team 47 Logo">
-        </div>
-        <div id="search-container"></div>
-        <button class="signin" style="text-align: center;">Sign In</button>
-        <div id="heart">
-            <i class="fas fa-heart"></i>
-        </div>
-        <div id="cart">
-            <i class="fas fa-shopping-cart"></i>
-        </div>
-    </div>
-    <nav>
-    <!-- The nav bar -->
-        <ul> 
-            <header class="navigation">
-        <div class="nav">
-          <li><a href="">Men</a></li>
-          <li><a href="">Women</a></li>
-          <li><a href="">Kids</a></li>
-          <li><a href="">Accessories</a></li>
-          <li><a href="">About Us</a></div></li>
-        </header>
-        </ul> 
-    </nav>
-
+<?php include 'navbar.php'?>
 
     <div id="container">
         <div id="section"><img src="image1.jpg" type="image" width="100%" height="100%" alt="code"/></div>

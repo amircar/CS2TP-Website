81a84d87ca3cdad3d768401f3ab0a57cd93bca5f
Hanin Nabeel <230087860@aston.ac.uk>
2024-11-29 11:47:00 +0000

    trapezoid fix. i created the trapezoid shapes and the button and it is fully functional

--- index.php
+++ index.php
@@ -48,16 +48,20 @@
 
         <div>
             <!-- FIXME categories section -->
-            <!-- <div id="section">
-                <div class="trapezoid1"><img src="" alt="category-image"></div>
-                <button class="trapezoid-button">MEN</button>
-                <div class="trapezoid2"><img src="" alt="category-image"></div>
-                <button class="trapezoid-button">WOMEN</button>
-                <div class="trapezoid3"><img src="" alt="category-image"></div>
-                <div class="trapezoid3-rectangle">
-                    <button class="trapezoid-button">KIDS</button>
-                </div>
-            </div> -->
+            <div class="trapezoid1"></div>
+            <div class="trapezoid1-rectangle">
+                <button class="trapezoid-button" style="text-align: center; clip-path: polygon(0% 0%, 100% 0%, 95% 100%, 0% 100%);">MEN</button>
+            </div>
+            
+            <div class="trapezoid2"></div>
+            <div class="trapezoid2-rectangle">
+                <button class="trapezoid-button" style="text-align: center; width: 315px;">WOMEN</button>
+            </div>
+
+            <div class="trapezoid3"></div>
+            <div class="trapezoid3-rectangle">
+            <button class="trapezoid-button" style="text-align: center; clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 5% 100%)">KIDS</button>
+            </div>
         </div>
 
         <?php require_once('footer.php');?>

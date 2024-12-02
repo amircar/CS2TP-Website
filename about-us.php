3f40e70cbaa0cfbb13df539a60275b8f00d98790
Hanin Nabeel <230087860@aston.ac.uk>
2024-12-02 17:12:17 +0000

    added some more stuff to the about us page like some text and i added three boxes for the images. i wnat to try and make the images be a rollover so i will try to explore and see what i can do. made some chages to the styles page and made sure text was alligned to the center.

--- about-us.php
+++ about-us.php
@@ -11,31 +11,51 @@
         <?php require_once('header.php');?>
         
         <main id="about-us-main">
-            <div id="section" style="height:400px; background-color:white;"></div> <!--add image -->
-            <div class="aboutus-box">
-                    <h3>About Us</h3>
+            <div id="section" style="justify-content: center; height:400px; background-color: white;">
+                <h3 style="text-align: center; margin-top: 150px;">About Us</h3>
+            </div> <!--add image -->
+            <div id="section" style="height:230px; justify-content: center; margin-top: 50px;">
+                <div class="aboutus-box" style="width: 70%; height: 200px;">
+                    <span>
+                        <h2 style="text-align: center; color: black;">Team 47</h2>
+                        <p style="text-align: center; color: black;">Team 47 is a passionate group 
+                            of young, driven individuals coming together to create a bold and 
+                            innovative clothing brand. United by a shared love for fashion and 
+                            creativity, we aim to redefine modern style with designs that inspire 
+                            confidence and individuality. Team 47 is more than just a brand—it's a 
+                            vision of empowerment, blending youthful energy with timeless aesthetics. 
+                            By combining quality craftsmanship with fresh, unique ideas, we're committed 
+                            to building a community that embraces self-expression through fashion. Join 
+                            us on this exciting journey as we bring Team 47 to life, one outfit at a time!
+                        </p>
+                    </span>
+                </div>
             </div>
-            <div id="section" style="height:300px;">
-                <span>
-                    <h2>text</h2>
-                    <p>text</p>
-                    <p>text</p>
-                    <p>text</p>
-                    <p>text</p>
-                </span>
-            </div>
-
+            <!-- TODO fix rollover -->
             <div class="content-separate"><div class="content-separate-box"></div></div>
-
-            <div class="about-us-2">
-                <!-- add 3 rollover images so when user hovers the mouse, 
-                text will appear and image will lose opacity a bit but still be visible. -->
-                <span>
-                    <h2>TITLE</h2>
-                    <p>TEXT</p>
-                </span>
-                <img src="" alt="about-us-image">
+            
+            <div id="section" style="height: 400px;">
+            <div class="imagebox">
+                    <div class="imagerollover">
+                        <img src="" alt="about-us-image">
+                            <div class="text2">
+                                <div class="aboutus-info">
+                                    <h4>About Us</h4>
+                                    <br></br>
+                                    aboutus team47 as a group
+                                </div>
+                            </div>
+                    </div>
+                </div>
             </div>
+            <!-- <div class="about-us-1">
+                 add 3 rollover images so when user hovers the mouse, 
+                text will appear and image will lose opacity a bit but still be visible. 
+ 
+                <img src="" alt="about-us-image">
+                <img src="" alt="about-us-image">
+                <img src="" alt="about-us-image">
+            </div> -->
 
             <div class="content-separate"><div class="content-separate-box"></div></div>
 

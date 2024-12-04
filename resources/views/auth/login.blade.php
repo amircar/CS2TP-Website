bd0c109f83210eca1679c8d87219064a3a76f1f9
amircar <amircar@hotmail.co.uk>
2024-12-04 11:20:40 +0000

    Login page + favicon
    Place favicon in correct folder and start log in page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,33 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en-GB">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
 
-@section('content')
+        <!----------------------------
+            SEO (optional)
+        ----------------------------->
+        <title>Team 47 Homepage</title>
+        <!-- title should be category/product name from database on single/products page -->
+        <meta name="description" content=""> <!-- shown under title on search engine, changes based on product (may not always be needed for pages that do not need to be found on search engine (like wishlist)-->
+        <!-- description = product desc -->
+        <meta name="robots" content="index, follow"> <!--for crawlers (no index, follow does not show up on search engine)-->
+        <!-- OPEN GRAPH (for social media) -->
+        <meta property="og:title" content="Team 47 Homepage">
+        <meta property="og:url" content="">
+        <meta property="og:description" content="">
+        <meta property="og:locale" content="en_GB">
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="Team 47">
+        <meta property="og:image" content=""> <!--url to image-->
+        <meta property="og:image:width" content="1200"> <!--min 1200 x 630-->
+        <meta property="og:image:height" content="630">
+    </head>
+
+    <body>
+         @include('header')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +97,5 @@
         </div>
     </div>
 </div>
-@endsection
+@include('footer');
+</body>

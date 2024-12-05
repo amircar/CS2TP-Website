336558c970dfd856db1ee74de3c94417ff9f3a97
ManrajBhangra <mgsbhangra@gmail.com>
2024-12-05 12:17:26 +0000

    Fix routing issues, add favicon and allow products to have separate pages
    There was issues with routing which meant links were to the incorrect page which were fixed. A favicon was added to all pages. The products page should now change depending on product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,111 +1,95 @@
 <!DOCTYPE html>
-<html lang="en-GB">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <!-- TODO add main js and style -->
-        <!-- TODO move single product js and css to main -->
-        <link rel="stylesheet" href={{ asset('css/Single-Product-Style.css') }}>
-        <script src={{ asset('js/Single-Product-Javascript.js') }}></script>
-        
-        <!-- TODO title should be "product name | category | Team 47" (category optional) -->
-        <title>Product Page</title>
-    </head>
+
+<html lang="en">
+<meta charset="UTF-8">
+<title>Product Page</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href={{asset('css/Single-Product-Style.css')}}>
+<script src={{asset('js/Single-Product-Javascript.js')}}></script>
+<link rel="icon" href="{{ asset('favicon.ico') }}">
+
 
     <body>
         @include('header')
 
-        <section id="singleProduct">
-            <div id="preview">
-                <div class="thumb">
-                    <img src="images/black T shirt 2.jpg">
-                </div>
-                <div class="thumb">
-                    <img src="images/black T shirt 3.jpg">
-                </div>
-                <div class="thumb">
-                    <img src="images/black T shirt 4.jpg">
-                </div>
-                <div class="thumb">
-                    <img src="images/black T shirt 5.jpg">
-                </div>
+
+<button id="back-to-top" type="button">TOP</button>
+
+<body>
+    <section id="singleProduct">
+        <div id="preview">
+            <div class="thumb">
+                <img src="images/black T shirt 2.jpg">
+            </div>
+            <div class="thumb">
+                <img src="images/black T shirt 3.jpg">
+            </div>
+            <div class="thumb">
+                <img src="images/black T shirt 4.jpg">
+            </div>
+            <div class="thumb">
+                <img src="images/black T shirt 5.jpg">
+            </div>
+        </div>
+
+        <div id="mainView">
+            <div class="thumbnail">
+                <img src="images/black T shirt.jpg">
+            </div>
+        </div>
+
+        <div id="productInfo">
+            <h1 class=productName>{{$product->name}}</h1>
+            <h4 class="price">£{{$product->price}}</h4>
+            <h4 class="colourTitle">COLOUR</h4>
+            <div class="colours">
+                <span class="colour red"></span>
+                <span class="colour yellow"></span>
+                <span class="colour blue"></span>
+                <span class="colour white"></span>
+                <span class="colour black"></span>
+            </div>
+            <h4 class="title">SIZE</h4>
+            <div class="sizes">
+            @foreach($product->sizes as $size)
+                <span class="size">{{$size->size}}</span>
+            @endforeach
             </div>
 
-            <div id="mainView">
-                <div class="thumbnail">
-                    <img src="images/black T shirt.jpg">
-                </div>
-            </div>
+            <a href="" class="Btn1">ADD TO BASKET</a>
+            <a href="" class="Btn2">ADD TO WISHLIST</a>
 
-            <div id="productInfo">
-                <h1 class=productName>Team 47 T Shirt</h1>
-                <h4 class="price">£29.99</h4>
-                <h4 class="colourTitle">COLOUR</h4>
-                <div class="colours">
-                    <span class="colour red"></span>
-                    <span class="colour yellow"></span>
-                    <span class="colour blue"></span>
-                    <span class="colour white"></span>
-                    <span class="colour black"></span>
-                </div>
-                <h4 class="title">SIZE</h4>
-                <div class="sizes">
-                    <span class="size">XS</span>
-                    <span class="size">S</span>
-                    <span class="size">M</span>
-                    <span class="size">L</span>
-                    <span class="size">XL</span>
-                </div>
-
-                <a href="" class="Btn1">ADD TO BASKET</a>
-                <a href="" class="Btn2">ADD TO WISHLIST</a>
-
-                <div id="basketBtn">
-
-                </div>
-
-                <div id="wishlistBtn">
-
-                </div>
+            <div id="basketBtn">
 
             </div>
-        </section>
 
-        <!-- Suggested Items /  Item carousel -->
-        <section id="suggestedChoices">
-            
-            <!-- TODO move to style.css -->
-            <div style="padding-left: 5%; font-size: 250%;">
-                You might also like...
+            <div id="wishlistBtn">
+
             </div>
-            <div style="height: 1px; background-color: black; margin-left: 5%; margin-right: 50%;"></div>
-            <div id="carouselContainer" style="max-width: 90%;">
-                <button class="carousel-button prev" onclick="moveCarousel(-1)">&#10094;</button>
-                <div id="carousel">
-                    <div class="carousel-item">
-                        <img src="images/black T shirt 2.jpg" alt="Suggested Product">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/black T shirt 3.jpg" alt="Suggested Product">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/black T shirt 2.jpg" alt="Suggested Product">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/black T shirt 3.jpg" alt="Suggested Product">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/black T shirt 2.jpg" alt="Suggested Product">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/black T shirt 3.jpg" alt="Suggested Product">
-                    </div>
-                </div>
-                <button class="carousel-button next" onclick="moveCarousel(1)">&#10095;</button>
+
+        </div>
+    </section>
+
+    <!-- Suggested Items /  Item carousel -->
+    <section id="suggestedChoices">
+        <div style="padding-left: 5%; font-size: 250%;">
+            You might also like...
+        </div>
+        <div style="height: 1px; background-color: black; margin-left: 5%; margin-right: 50%;"></div>
+        <div id="carouselContainer" style="max-width: 90%;">
+            <button class="carousel-button prev" onclick="moveCarousel(-1)">&#10094;</button>
+            <div id="carousel">
+                @foreach ($products as $product)
+                    @foreach ($product->product_images as $image)
+                        <div class="carousel-item">
+                            @if ($image->is_primary)
+                            <img src={{asset($image->image_url)}} alt="Suggested Product">    
+                            @endif
+                        </div>
+                    @endforeach
+                @endforeach
             </div>
         </section>
-
         @include('footer')
     </body>
 </html>

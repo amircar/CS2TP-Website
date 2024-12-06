2351e586d8017f0ee2d7575c49eb01a74039e88a
shuraimhanif <shuraimhanif99@gmail.com>
2024-12-06 23:46:11 +0000

    integrating the contact-us page into the most recent main branch commit
    added the route, controller and view for the contact us page into laravel

--- storage/framework/views/07010a8c2a457abe95b1e6cddb88a4cb.php
+++ storage/framework/views/07010a8c2a457abe95b1e6cddb88a4cb.php
@@ -4,17 +4,22 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/style.css')); ?>">
-        <script type="text/javascript" src="<?php echo e(asset('js/main.js')); ?>"></script>
+        <script defer type="text/javascript" src="<?php echo e(asset('js/main.js')); ?>"></script>
+        <title>About Team 47</title>
     </head>
 
     <body>
-    <?php echo $__env->make('header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
+        <?php echo $__env->make('header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         
         <main id="about-us-main">
-            <div id="section" style="height:400px; background-color:white;"></div> <!--add image -->
-            <div class="aboutus-box">
-                    <h3>About Us</h3>
+
+            <!-- FIXME move css to main -->
+            <div id="section" style="height:400px; background-color:white;"></div>
+            <div class="about-us-box">
+                <h3>About Us</h3>
             </div>
+
+            <!-- FIXME move css to main -->
             <div id="section" style="height:300px;">
                 <span>
                     <h2>text</h2>
@@ -28,6 +33,7 @@
             <div class="content-separate"><div class="content-separate-box"></div></div>
 
             <div class="about-us-2">
+                <!-- TODO ?part of mvp? -->
                 <!-- add 3 rollover images so when user hovers the mouse, 
                 text will appear and image will lose opacity a bit but still be visible. -->
                 <span>
@@ -47,6 +53,7 @@
                 </span>
             </div>
         </main>
-        <?php echo $__env->make('footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
+
+        <?php echo $__env->make('footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     </body>
 </html><?php /**PATH C:\xampp\htdocs\CS2TP-Website\resources\views/about-us.blade.php ENDPATH**/ ?>
--- storage/framework/views/e9307e8406cde212a1eb69b9c6397819.php
+++ storage/framework/views/e9307e8406cde212a1eb69b9c6397819.php
@@ -3,44 +3,32 @@
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/style.css')); ?>">
-        <script type="text/javascript" src="<?php echo e(asset('js/main.js')); ?>"></script>
+        <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+        <script defer type="text/javascript" src="<?php echo e(asset('js/main.js')); ?>"></script>
+        <link rel="icon" href="<?php echo e(asset('favicon.ico')); ?>">
+        <title>Shop for Men's Clothing | Team 47</title>
     </head>
 
     <body>
-    <?php echo $__env->make('header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
+        <?php echo $__env->make('header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
-        <main id="mens-main">
+        <!-- TODO men's paragraph -->
+        <main id="men-main">
             <h2>MEN'S FASHION</h2>
             <p>introductory paragraph on mens fashion</p>
         </main>
 
         <div class="content-separate"><div class="content-separate-box"></div></div>
 
-        <!-- TODO slideshow of products -->
-        <div id="featured-products-list">
-            <h2>FEATURED PRODUCTS</h2>
+        <section id="featured-products-list">
+            <h2>FEATURED IN MEN'S</h2>
             <ul class="products-list">
-                <?php echo $__env->make('test', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
-                <?php echo $__env->make('single-product', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-                <?php echo $__env->make('single-product', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-                <?php echo $__env->make('single-product', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                <?php $__currentLoopData = $products4; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <?php echo $__env->make('single-product' , ['product' => $product], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
             </ul>
-        </div>
+        </section>
 
-        <div class="content-separate"><div class="content-separate-box"></div></div>
-
-        <div id="featured-products-list">
-            <h2>FEATURED PRODUCTS</h2>
-            <ul class="products-list">
-                <?php include('single-product');?>
-                <?php include('single-product');?>
-                <?php include('single-product');?>
-                <?php include('single-product');?>
-            </ul>
-        </div>
-
-        <?php echo $__env->make('footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
+        <?php echo $__env->make('footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     </body>
-</html>
-        <?php /**PATH C:\xampp\htdocs\CS2TP-Website\resources\views/mens.blade.php ENDPATH**/ ?>
+</html><?php /**PATH C:\xampp\htdocs\CS2TP-Website\resources\views/mens.blade.php ENDPATH**/ ?>

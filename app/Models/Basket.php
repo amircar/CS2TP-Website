09e37e6315bafffd6cbee1253c0c80aba712e971
ManrajBhangra <mgsbhangra@gmail.com>
2025-01-29 10:17:40 +0000

    Add to basket
    Fixed add to basket function when user doesn't have a basket already

--- app/Models/Basket.php
+++ app/Models/Basket.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Basket extends Model
 {
     protected $table = 'baskets'; //Directs to correct table
+    public $timestamps = false;
+
+    protected $fillable = [ //Declares which fields can be stated when a new basket is made
+        'user_id'
+    ];
     
     public function stocks()//Declares relationship to stocks as many-many. Allows retrieval of quantity column from basket_stock
     {

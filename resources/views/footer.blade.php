b44233ebf0d74ebf67c41229254ca76fa1641194
shuraimhanif <shuraimhanif99@gmail.com>
2025-03-19 10:47:04 +0000

    footer login register updated
    updated footer buttons to take you to login and signup pages when clicked

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -8,8 +8,8 @@
             <div class="footer-section">  
                 <h3>TEAM 47</h3>
                 <p>Sign up or log in so that you can make purchases and keep up to date with the latest offers.</p>
-                <button class="footer-button">Log in</button>
-                <button class="footer-button">Sign Up</button>
+                <a href="{{ route('login') }}"><button class="footer-button">Log in</button></a>
+                <a href="{{ route('register') }}"><button class="footer-button">Sign Up</button></a>
             </div>
             <div id="footer-space"></div>
         @endguest
